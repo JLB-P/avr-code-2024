a822fb507d122462428f3d54e336971a05aac15b
JLB-P <joseluis.barbosap@gmail.com>
2024-03-12 18:34:34 -0600

    upd120324

--- sdk_at328p/sdk_at328p/i2c.c
+++ sdk_at328p/sdk_at328p/i2c.c
@@ -14,7 +14,7 @@ void init_i2c(void)
 	TWSR = ((0 << TWPS1) & (0 << TWPS0)); //Prescaler = 1
 	TWBR = 0X14; //Define Bit rate SCL_frec=CPU_frec/(16+2(TWBR)x4^(prescaler))
 				//SCL_Frec=(16000000/(16+2(20)(4)))=74Khz
-	TWCR = (1<<TWEN); //Activa la interfaz TWI
+	TWCR = (1<<TWEN); //Enable TWI
 }
 
 void start(void)
@@ -32,7 +32,7 @@ void stop(void)
 void write_i2c(uint8_t data)
 {
 	TWDR = data; //Byte to write
-	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA); //writing with aknokledge (TWEA=1)
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA); //writing with acknowledge (TWEA=1)
 	while((TWCR & (1<<TWINT))==0); //wait until TWINT=0 (TWI then finish)
 }
 
